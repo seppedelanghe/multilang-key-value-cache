590745e5a90c6667643d9bf592f21a4d046293da
Seppe De Langhe <seppe.delanghe@intelliprove.com>
2023-03-31 17:06:55 +0200

    GO: use map instead of array for cache

--- go/main.go
+++ go/main.go
@@ -11,13 +11,7 @@ import (
 )
 
 var port = "9800"
-
-type item struct {
-	key   string
-	value []byte
-}
-
-var cache = []item{}
+var cache = make(map[string][]byte)
 
 func getKey(url string) (string, error) {
 	s := strings.Split(url, "/")
@@ -26,26 +20,6 @@ func getKey(url string) (string, error) {
 	}
 
 	return s[1], nil
-}
-
-func findKey(key string) (item, error) {
-	for i := range cache {
-		if cache[i].key == key {
-			return cache[i], nil
-		}
-	}
-	return item{}, errors.New("key not found")
-}
-
-func removeFromCache(key string) bool {
-	for i := range cache {
-		if cache[i].key == key {
-			cache[i] = cache[len(cache)-1]
-			cache = cache[:len(cache)-1]
-			return true
-		}
-	}
-	return false
 }
 
 func streamToByteArray(stream io.Reader) ([]byte, error) {
@@ -57,62 +31,61 @@ func streamToByteArray(stream io.Reader) ([]byte, error) {
 	return buf.Bytes(), nil
 }
 
-func get(w http.ResponseWriter, r *http.Request) {
+func handleGet(w http.ResponseWriter, r *http.Request) {
 	uuid, err := getKey(r.URL.Path)
 	if err != nil {
 		fmt.Printf("Error in url: %s\n", err)
 		w.WriteHeader(404)
 	} else {
-		x, nf := findKey(uuid)
-		if nf != nil {
-			w.WriteHeader(404)
-		} else {
+		value, exists := cache[uuid]
+		if exists {
 			w.WriteHeader(200)
-			w.Write(x.value)
+			w.Write(value)
+		} else {
+			w.WriteHeader(404)
 		}
 	}
 }
 
-func post(w http.ResponseWriter, r *http.Request) {
+func handlePost(w http.ResponseWriter, r *http.Request) {
 	uuid, err := getKey(r.URL.Path)
 	if err != nil {
 		fmt.Printf("Error in url: %s\n", err)
 		w.WriteHeader(404)
 	} else {
-		x := new(item)
-		x.key = uuid
 		buf, rerr := streamToByteArray(r.Body)
 		if rerr != nil {
 			w.WriteHeader(500)
 		} else {
-			x.value = buf
-			cache = append(cache, *x)
+			cache[uuid] = buf
 			w.WriteHeader(201)
 		}
 	}
 }
 
-func delete(w http.ResponseWriter, r *http.Request) {
+func handleDelete(w http.ResponseWriter, r *http.Request) {
 	uuid, err := getKey(r.URL.Path)
 	if err != nil {
 		fmt.Printf("Error in url: %s\n", err)
 		w.WriteHeader(404)
 	} else {
-		ok := removeFromCache(uuid)
-		if !ok {
+		_, exists := cache[uuid]
+		if exists {
+			delete(cache, uuid)
+			w.WriteHeader(200)
+		} else {
 			w.WriteHeader(404)
 		}
-		w.WriteHeader(200)
 	}
 }
 
 func handle(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
-		get(w, r)
+		handleGet(w, r)
 	} else if r.Method == "POST" {
-		post(w, r)
+		handlePost(w, r)
 	} else if r.Method == "DELETE" {
-		delete(w, r)
+		handleDelete(w, r)
 	} else {
 		w.WriteHeader(405)
 		r.Body.Close()
