2962fc19e76a65717ac77537b7ed95278e6ede12
Seppe De Langhe <seppe.delanghe@intelliprove.com>
2023-08-17 14:08:11 +0200

    ♻️  set kick default to true

--- python3/main.py
+++ python3/main.py
@@ -10,7 +10,7 @@ PORT = int(os.environ.get('MKV_PORT', '9800'))
 MAX_SIZE = int(os.environ.get('MKV_MAX_SIZE', '1000'))
 MAX_MEMORY_USAGE = float(os.environ.get('MKV_MAX_MEMORY', '0.9'))
 MAX_MEMORY_USAGE = int(MAX_MEMORY_USAGE) if int(MAX_MEMORY_USAGE) == MAX_MEMORY_USAGE else MAX_MEMORY_USAGE
-KICK = True if os.environ.get('MKV_KICK', 'false').lower() == 'true' else False
+KICK = True if os.environ.get('MKV_KICK', 'true').lower() == 'true' else False
 VERIFY = True if os.environ.get('MKV_VERIFY', 'false').lower() == 'true' else False
 
 cache = Cache(MAX_SIZE, KICK, VERIFY, MAX_MEMORY_USAGE)
